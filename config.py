399aebdd3f47259c1fea7adb3589333bec0d6145
Imlapta <113951333+Imlapta@users.noreply.github.com>
2025-01-20 23:50:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "18618422"))
-API_HASH = getenv("API_HASH", "f165b1caec3cfa4df943fe1cbe82d22a")
-BOT_TOKEN = getenv("BOT_TOKEN", "7064501216:AAEOQMaqLGL5e6TBau-8dYhRxUk5yrG3738")
-OWNER_ID = int(getenv("OWNER_ID", "6664582540"))
+API_ID = int(getenv("API_ID", "3737117"))
+API_HASH = getenv("API_HASH", "f466ca6ff400954d192ce0992ddf8b5d")
+BOT_TOKEN = getenv("BOT_TOKEN", "1794688698:AAFDoYKCbL5xHgduCK6sOQKaUC7P17L3UvU")
+OWNER_ID = int(getenv("OWNER_ID", "1110590703"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050277919 2112898623").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URL = getenv("MONGO_DB", "mongodb+srv://rrahuldhaker:7F6evbaKu8m7Ybr5@cluster0.w5kvs.mongodb.net/?retryWrites=true&w=majority")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002034072106"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002034072106"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002247262105"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002247262105"))
 
 """
 #
